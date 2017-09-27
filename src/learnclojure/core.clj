c883dfd60fb5825d5827a33ee6e78697223ccc57
Andreas Vogler <andreas.vogler@me.com>
2017-09-27 14:20:38 +0200

    udpate

--- src/learnclojure/core.clj
+++ src/learnclojure/core.clj
@@ -3,27 +3,44 @@
   (:require [clojure.core.async :refer [chan go go-loop >! <!] :as async]
             [learnclojure.matrix :as matrix]))
 
-(def input-neurons [[1]
-                    [2]])
+(defn activation-fn [x]
+  "activation function"
+  (Math/tanh x))
 
-(def input-hidden-strengths [[1 2]
-                             [3 4]
-                             [5 6]])
+(defn dactivation-fn [y]
+  "derivation of activation function"
+  (- 1.0 (* y y)))
 
-(def hidden-output-strengths [[0.15 0.16 0.17]
-                              [0.02 0.03 0.04]])
-
-(def activation-fn (fn [x] (Math/tanh x)))
-;(def activation-fn (fn [x] x))
-
-(def dactivation-fn (fn [y] (- 1.0 (* y y))))
-
-(defn layer-activation [inputs strengths]
+(defn layer-activation [inputs weights]
   "forward propagate the input of a layer"
     (mapv #(mapv activation-fn %)
-        (matrix/mmul strengths inputs)))
+        (matrix/mmul weights inputs)))
 
-(matrix/mmul input-hidden-strengths input-neurons)
+(defn output-deltas [outputs targets]
+  "calculate the delta errors for the output layer (desired value - actual value)"
+  (mapv * (mapv dactivation-fn outputs) (mapv - targets outputs)))
 
-(def hidden-neurons (layer-activation input-neurons input-hidden-strengths))
-(layer-activation hidden-neurons hidden-output-strengths)
+;------------------------------------------------------------------------------------------
+(defn matrix-of [array]
+  "create a matrix from an array [1 2 3] => [[1] [2] [3]]"
+  (mapv #(vector %) array))
+
+;------------------------------------------------------------------------------------------
+(def input-neurons [0.1, 0.2])
+
+(def input-hidden-weights [[1 2]
+                           [3 4]
+                           [5 6]])
+
+(def hidden-output-weights [[0.15 0.16 0.17]
+                            [0.02 0.03 0.04]])
+
+(def targets [0.2, 0.3])
+
+(def hidden-neurons (layer-activation (matrix-of input-neurons) input-hidden-weights))
+(def output-neurons (layer-activation hidden-neurons hidden-output-weights))
+
+(output-deltas (flatten output-neurons) targets)
+
+
+
