a6abf517fab37069e6aa9f2ae5b417c7a8713cb4
vogler75 <bmw320ix>
2017-10-28 22:36:26 +0200

    update

--- src/learnclojure/neural.clj
+++ src/learnclojure/neural.clj
@@ -1,4 +1,4 @@
-(ns learnclojure.neuronal
+(ns learnclojure.neural
   (:import [java.util Date Calendar])
   (:require [clojure.core.async :refer [chan go go-loop >! <!] :as async]
             [learnclojure.matrix :as matrix]))
@@ -23,20 +23,31 @@
 ;------------------------------------------------------------------------------------------
 (defn layer-calculation [inputs weights]
   "forward propagate the input of a layer"
-    (mapv #(mapv activation-fn %)
+  ; inputs [[1] [0]]
+  ; weights [[0.12 0.01] [0.2 0.02] [0.13 0.03]]
+  (mapv #(mapv activation-fn %)
         (matrix/mmul weights inputs)))
 
 (defn output-errors-calculation [outputs targets]
   "calculate the delta errors for the output layer (target value - actual value)"
-  (mapv * (mapv dactivation-fn outputs) (mapv - targets outputs)))
+  ; outputs [[0.02] [0.027]]
+  ; targets [0 1]
+  (let [outputs (flatten outputs) targets (flatten targets)]
+    (mapv * (mapv dactivation-fn outputs) (mapv - targets outputs))))
 
 (defn hidden-errors-calculation [errors weights]
   "calculate errors for the hidden layer based on the output-errors and weights"
+  ; errors [0.8 0.5]
+  ; weights [[0.15 0.02 0.01] [0.16 0.03 0.02]]
   (flatten (matrix/mmul (matrix/transpose weights) (matrix errors))))
 
-(defn update-weights-calculation [neurons weights errors rate]
+(defn weights-calculation [neurons errors weights rate]
   "update/adjust the weights of a hidden layer according to the errors"
-  (:tbd))
+  ; neurons [[0.02] [0.02]]
+  ; errors [0.8 0.5]
+  ; weights [[0.15 0.02 0.01] [0.16 0.03 0.02]]
+  (let [weight-changes (map * errors (flatten neurons))] ; (0.018520152042568426 0.013804030750041782)
+    (map (fn [xs w] (map (fn [x] (+ x w)) xs)) weights weight-changes)))
 
 ;------------------------------------------------------------------------------------------
 ; Example 1
@@ -90,6 +101,10 @@
 (def output-errors (output-errors-calculation (flatten output-neurons) targets))
 (def hidden-errors (hidden-errors-calculation output-errors hidden-output-weights))
 
+(def hidden-output-weights-new (weights-calculation output-neurons output-errors hidden-output-weights learning-rate))
+(def input-hidden-weights-new (weights-calculation hidden-neurons hidden-errors input-hidden-weights learning-rate))
+
+
 hidden-neurons
 ;=> [[0.11942729853438588]
 ;    [0.197375320224904]
@@ -99,22 +114,8 @@ output-neurons
 ;=> [[0.02315019005321053]
 ;    [0.027608061500083565]]
 
-(def output-errors [0.8 0.5])
 output-errors
 ;=> [-0.023137783141771645 0.9716507764442904]
 
 hidden-errors
 ;=> [0.14982559238071416 0.027569216735265096 0.018880751432503236]
-
-
-(def neurons hidden-neurons)
-(def weights hidden-output-weights)
-(def errors output-errors)
-(def rate 0.2)
-
-(def neurons [[0.4] [0.5]])
-neurons
-errors
-weights
-
-(mapv #(* errors %) (flatten neurons))
